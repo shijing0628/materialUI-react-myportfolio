ff1485a2e7eadc4ca17fbe42236800f299a76552
Shijing Liu <brendaliu0628@gmail.com>
2020-10-23 15:37:31 -0500

    Footer component is completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import { Typography } from '@material-ui/core';
+import React from 'react';
+import './Footer.css'
+import resumeData from '../../utils/resumeData'
+
 
 const Footer = () => {
  return (
-  <div>
-   Footer
+  <div className='footer'>
+   <div className="footer_left">
+    <Typography className="footer_name">{resumeData.name}</Typography>
+   </div>
+   <div className="footer_right">
+    <Typography className="footer_copyright">
+     Design and developed by {' '}<a href='/'>Brenda Liu</a>
+     <br />
+     Reference from <a href="http://tavonline.net/html/mat/default/index.html#" target="_blank">Tavonline</a>
+    </Typography>
+   </div>
   </div>
  )
 }
